a6c6c0c9266f4446190ec1054286ec654422629a
Joseph Kogut <joseph@balena.io>
2022-08-15 17:00:43 -0700

    entry: setup loopback and metadata devices
    Change-type: minor
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- entry.sh
+++ entry.sh
@@ -6,10 +6,11 @@ if [ "${WORKER_TYPE}" != "qemu" ]; then
 	exec node ./build/bin
 fi
 
+misc_major=10
 tun_minor=$(grep tun /proc/misc | cut -d ' ' -f1)
 if [ -n "${tun_minor}" ]; then
 	mkdir -p /dev/net
-	if ! mknod -m 666 /dev/net/tun c 10 "${tun_minor}"; then
+	if ! mknod -m 666 /dev/net/tun c "${misc_major}" "${tun_minor}"; then
 		echo "Unable to create TUN device node"
 		exit 1
 	fi
@@ -21,11 +22,29 @@ fi
 
 kvm_minor=$(grep kvm /proc/misc | cut -d ' ' -f1)
 if [ -n "${kvm_minor}" ]; then
-	if ! mknod -m 666 /dev/kvm c 10 "${kvm_minor}"; then
+	if ! mknod -m 666 /dev/kvm c "${misc_major}" "${kvm_minor}"; then
 		echo "Unable to create KVM device node, software emulation is still available"
 	fi
 else
 	echo "KVM is unavailable, falling back on software emulation"
 fi
+
+loop_major=7
+loopctrl_minor=$(grep loop-control /proc/misc | cut -d ' ' -f1)
+if [ -n "${loopctrl_minor}" ]; then
+	if ! mknod -m 660 /dev/loop-control c "${misc_major}" "${loopctrl_minor}"; then
+		echo "Unable to create loop-control device node"
+		exit 1
+	fi
+
+	for i in $(seq 0 128); do
+		mknod -m 660 "/dev/loop${i}" b "${loop_major}" "${i}"
+	done
+fi
+
+metadata_major=9
+for i in $(seq 0 127); do
+	mknod -m 660 "/dev/md${i}" b "${metadata_major}" "${i}"
+done
 
 node ./build/bin
