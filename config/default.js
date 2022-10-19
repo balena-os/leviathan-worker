5f332a9f1cb4745b83b7c84c766924b06ead4f16
Joseph Kogut <joseph@balena.io>
2022-10-19 11:32:33 -0700

    config: default: standardize boolean parsing
    Change-type: patch
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- config/default.js
+++ config/default.js
@@ -1,3 +1,9 @@
+const isTrue = (v, def=false) => {
+	return v != undefined
+		? ['true', '1'].includes(v)
+		: def
+};
+
 module.exports = {
 	worker: {
 		port: process.env.WORKER_PORT || 80,
@@ -6,7 +12,7 @@ module.exports = {
 				workdir: process.env.WORKDIR || '/data',
 				deviceType: process.env.WORKER_TYPE || 'testbot_hat',
 			},
-			screenCapture: process.env.SCREEN_CAPTURE === 'true',
+			screenCapture: isTrue(process.env.SCREEN_CAPTURE),
 			network: {
 				wired: process.env.NETWORK_WIRED_INTERFACE || 'eth1',
 				wireless: process.env.NETWORK_WIRELESS_INTERFACE || 'wlan0',
@@ -15,10 +21,10 @@ module.exports = {
 				architecture: process.env.QEMU_ARCH || 'x86_64',
 				cpus: process.env.QEMU_CPUS || '4',
 				memory: process.env.QEMU_MEMORY || '512M',
-				debug: process.env.QEMU_DEBUG || false,
-				forceRaid: process.env.QEMU_FORCE_RAID || false,
+				debug: isTrue(process.env.QEMU_DEBUG),
+				forceRaid: isTrue(process.env.QEMU_FORCE_RAID),
 				network: {
-					autoconfigure: true,
+					autoconfigure: isTrue(process.env.QEMU_NETWORK_AUTOCONFIGURE, true),
 					bridgeName: process.env.QEMU_BRIDGE_NAME || null,
 					bridgeAddress: process.env.QEMU_BRIDGE_ADDRESS || null,
 					dhcpRange: process.env.QEMU_DHCP_RANGE || null,
